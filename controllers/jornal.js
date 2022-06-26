cfce04093bd8bc1b2b88d33b2163d9018d3e0cbc
danielecollins <57637750+danielecollins@users.noreply.github.com>
2022-06-25 22:50:38 -0600

    Added error handling to my jornal route

--- controllers/jornal.js
+++ controllers/jornal.js
@@ -1,22 +1,26 @@
 const db = require('../models');
 const Jornal = db.jornal;
 
-exports.create = (req, res) => {
+module.exports.create = (req, res) => {
+  try {
     if (!req.body.username || !req.body.entry || !req.body.entryDate) {
-        res.status(400).send({ message: 'Content can not be empty!' });
-        return;
+      res.status(400).send({ message: 'Content can not be empty!' });
+      return;
     }
 
     const jornal = new Jornal(req.body);
     jornal
-        .save()
-        .then((data) => {
-            console.log(data);
-            res.status(201).send(data);
-          })
-          .catch((err) => {
-            res.status(500).send({
-              message: err.message || 'Some error occurred while creating the jornal entry.'
-            });
-          });
+      .save()
+      .then((data) => {
+        console.log(data);
+        res.status(201).send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Some error occurred while creating the jornal entry.'
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
