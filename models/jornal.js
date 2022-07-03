9a3f69230775f132e0deceb12c80a4315594c5d0
danielecollins <57637750+danielecollins@users.noreply.github.com>
2022-07-02 23:38:20 -0600

    Removed the object id from the jornal model

--- models/jornal.js
+++ models/jornal.js
@@ -1,10 +1,5 @@
-const { ObjectId } = require("mongodb");
-
 module.exports = (mongoose) => {
     const jornalSchema = mongoose.Schema({
-      _id: {
-        type: ObjectId
-      },
       username: {
         type: String
       },
