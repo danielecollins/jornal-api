b283de2cb7ddbc153ae9d38ad50c74a0e23eab96
danielecollins <57637750+danielecollins@users.noreply.github.com>
2022-07-03 22:03:38 -0600

    add the entry route

--- routes/jornal.js
+++ routes/jornal.js
@@ -5,7 +5,7 @@ const jornalController = require('../controllers/jornal');
 
 router.post('/', jornalController.create);
 router.get('/:username', jornalController.getAllEntriesForUser);
-router.get('/entryDate', jornalController.)
+router.get('/entryDate', jornalController.getEntryByDate);
 router.put('/:id', jornalController.updateEntry);
 router.delete('/:id', jornalController.deleteEntry);
 
